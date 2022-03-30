0e9d67431446d256d83f4006a6925cf0b3e3efb5
Admin <Admin@DESKTOP-Q9C1DBG>
2022-03-31 01:34:00 +0530

    Add project files.

---
+++ Array_practice/Program.cs
@@ -0,0 +1,30 @@
+﻿class fibonnaci
+{ 
+    
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter no. of fabonnacci series ");
+        int s = int.Parse(System.Console.ReadLine());
+
+        int n1 = 0;
+        int n2 = 1;
+        System.Console.WriteLine(n1);
+        System.Console.WriteLine(n2);
+        
+        for(int n3 =2; n3 <=s; n3++)
+        {
+            n3 = n1 + n2;
+            System.Console.WriteLine(n3);
+
+
+            n1 = n2;
+
+            n2 = n3;
+            
+            
+
+        }
+
+    }
+
+}
